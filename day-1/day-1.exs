6a179a7ff4b3142c64255cef0d1aed577aac4f28
IsacEkeroth <isac.ekeroth@icloud.com>
2024-12-03 22:09:44 +0100

    day 1 pt 2

--- day-1/day-1.exs
+++ day-1/day-1.exs
@@ -1,7 +1,5 @@
-import Enum
-
 defmodule Day1 do
-  def run() do
+  def run_pt1() do
     "input.txt"
     |> read_file()
     |> parse_input()
@@ -9,6 +7,15 @@ defmodule Day1 do
     |> calculate_distances()
     |> total_distance()
     |> IO.inspect(label: "Total Distance")
+  end
+
+  def run_pt2() do
+    "input.txt"
+    |> read_file()
+    |> parse_input()
+    |> pair_numbers()
+    |> calculate_similarity()
+    |> IO.inspect(label: "similarity")
   end
 
   defp read_file(filename) do
@@ -30,22 +37,33 @@ defmodule Day1 do
 
   defp split_input(line) do
     [x, y] = String.split(line, " ", trim: true)
-    {x, y}
+    {String.to_integer(x), String.to_integer(y)}
   end
 
-  defp pair_numbers(pairs) do
-    {xs, ys} = Enum.unzip(pairs)
+  defp pair_numbers(pair) do
+    {xs, ys} = Enum.unzip(pair)
     {Enum.sort(xs), Enum.sort(ys)}
   end
 
   defp calculate_distances({xs, ys}) do
     Enum.zip(xs, ys)
-    |> Enum.map(fn {x, y} -> abs(String.to_integer(x) - String.to_integer(y)) end)
+    |> Enum.map(fn {x, y} -> abs(x - y) end)
   end
 
   defp total_distance(distances) do
     Enum.sum(distances)
   end
+
+  defp calculate_similarity({xs, ys}) do
+    Enum.reduce(xs, 0, fn x, acc ->
+      acc + count_matches(ys, x) * x
+    end)
+  end
+
+  defp count_matches(ys, x) do
+    Enum.count(ys, fn y -> y == x end)
+  end
 end
 
-Day1.run()
+Day1.run_pt1()
+Day1.run_pt2()

9b4333157935b5cca3afa899a945b11cc6f8ff3c
IsacEkeroth <isac.ekeroth@icloud.com>
2024-12-03 21:19:54 +0100

    Refactor Day1 module: streamline file reading and processing flow

--- day-1/day-1.exs
+++ day-1/day-1.exs
@@ -1,49 +1,50 @@
 import Enum
 
 defmodule Day1 do
-  def get_input() do
-    case File.read("input.txt") do
+  def run() do
+    "input.txt"
+    |> read_file()
+    |> parse_input()
+    |> pair_numbers()
+    |> calculate_distances()
+    |> total_distance()
+    |> IO.inspect(label: "Total Distance")
+  end
+
+  defp read_file(filename) do
+    case File.read(filename) do
       {:ok, content} ->
         content
-        |> String.split("\n", trim: true)
-        |> Enum.map(&split_input/1)
 
       {:error, reason} ->
         IO.puts("Error reading file: #{reason}")
-        []
+        ""
     end
   end
 
-  defp split_input(xs) do
-    parts = String.split(xs, " ", trim: true)
-    {hd(parts), hd(tl(parts))}
+  defp parse_input(content) do
+    content
+    |> String.split("\n", trim: true)
+    |> Enum.map(&split_input/1)
   end
 
-  def pair_number(pairs) do
-    {xs, ys} = unzip(pairs)
-    sorted_xs = sort(xs)
-    sorted_ys = sort(ys)
-    {sorted_xs, sorted_ys}
+  defp split_input(line) do
+    [x, y] = String.split(line, " ", trim: true)
+    {x, y}
   end
 
-  def calc_distance({xs, ys}) do
+  defp pair_numbers(pairs) do
+    {xs, ys} = Enum.unzip(pairs)
+    {Enum.sort(xs), Enum.sort(ys)}
+  end
+
+  defp calculate_distances({xs, ys}) do
     Enum.zip(xs, ys)
     |> Enum.map(fn {x, y} -> abs(String.to_integer(x) - String.to_integer(y)) end)
   end
 
-  def total_distance(xs) do
-    sum(xs)
-  end
-
-  def run() do
-    get_input()
-    # |> IO.inspect(label: "Input", limit: 10)
-    |> pair_number()
-    # |> IO.inspect(label: "Pairs", limit: 10)
-    |> calc_distance()
-    # |> IO.inspect(label: "Distances", limit: 10)
-    |> total_distance()
-    |> IO.inspect(label: "Total Distance")
+  defp total_distance(distances) do
+    Enum.sum(distances)
   end
 end
 

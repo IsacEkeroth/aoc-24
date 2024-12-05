1e0d5e46d47e60403da6d224811b8edc8f8d2d4d
IsacEkeroth <isac.ekeroth@icloud.com>
2024-12-05 17:10:56 +0100

    wip day 3

--- day-3/day-3.exs
+++ day-3/day-3.exs
@@ -1,8 +1,10 @@
 defmodule Day3 do
-  def run_pt1() do
+  def run() do
     "input_whole.txt"
     |> read_file()
     |> parse_input()
+    # |> IO.inspect()
+    |> calc_switch()
     |> IO.inspect(label: "output")
   end
 
@@ -18,16 +20,31 @@ defmodule Day3 do
   end
 
   defp parse_input(content) do
-    regex_mul = ~r/mul\(\d{1,3},\d{1,3}\)/
-    regex_numbers = ~r/\d{1,3}/
+    regex_mul = ~r/(mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\))/
 
     Regex.scan(regex_mul, content)
-    |> Enum.map(&(Regex.scan(regex_numbers, hd(&1)) |> Enum.concat()))
-    |> Enum.reduce(
-      0,
-      fn x, acc -> acc + String.to_integer(hd(x)) * String.to_integer(hd(tl(x))) end
-    )
+    |> Enum.concat()
   end
+
+  defp multiply_numbers([x, y | []]) do
+    String.to_integer(x) * String.to_integer(y)
+  end
+
+  defp calc_mul(x) do
+    regex_numbers = ~r/\d{1,3}/
+
+    Regex.scan(regex_numbers, x)
+    |> Enum.concat()
+    |> multiply_numbers()
+  end
+
+  defp calc_switch(xs), do: calc_switch(xs, :do)
+
+  defp calc_switch([], _), do: 0
+  defp calc_switch(["do()" | xs], _), do: calc_switch(xs, :do)
+  defp calc_switch(["don't()" | xs], _), do: calc_switch(xs, :dont)
+  defp calc_switch([x | xs], :do), do: calc_mul(x) + calc_switch(xs, :do)
+  defp calc_switch([_x | xs], :dont), do: calc_switch(xs, :dont)
 end
 
-Day3.run_pt1()
+Day3.run()
